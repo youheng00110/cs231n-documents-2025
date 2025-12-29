bef6a080fdcb475989b243d300a2d2d37de13859
FishDragon <3037486989@qq.com>
2025-12-29 12:10:07 +0800

    Remove large dataset files and add .gitignore

--- assignment-Chinese/assignment2/cs231n/datasets/get_coco_dataset_Windows.sh
+++
@@ -1,7 +0,0 @@
-#Windows系统
-#!/bin/bash
-if [ ! -d "coco_captioning" ]; then
-    curl -O "http://cs231n.stanford.edu/coco_captioning.zip"
-    unzip coco_captioning.zip
-    rm coco_captioning.zip
-fi
